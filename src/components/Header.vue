29417aeb4efbb685558c151f7018baac1d947d89
depgeinfdeveloper <depgeinf.developer@gmail.com>
2022-04-16 19:51:43 -0500

    iniciando proyecto

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand">Crypto Index</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-aujto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a href="#" class="nav-link activate" aria-current="page">Trendig Listing</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link" aria-current="page">Exchanges</a>
+              </li>
+               <li class="nav-item">
+                <a href="#" class="nav-link" aria-current="page">Portfolio</a>
+              </li>
+          </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "node_modules/bootstrap/scss/_functions.scss";
+        @import "node_modules/bootstrap/scss/_variables.scss";
+        `
+      }
+    }
+  },
+  lintOnSave: false
 })
